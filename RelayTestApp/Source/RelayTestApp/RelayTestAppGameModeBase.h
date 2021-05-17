208f05ed789366b3ca52fca74244a9572f44da58
franco <82105876+francobithead@users.noreply.github.com>
2021-05-17 14:19:07 -0400

    Project Created + added git ignore properties + added a gitattributes

---
+++ RelayTestApp/Source/RelayTestApp/RelayTestAppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RelayTestAppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RELAYTESTAPP_API ARelayTestAppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
