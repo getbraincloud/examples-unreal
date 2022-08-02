29fbe7d2d99cca4bc956cb9329c51b0d9ff940a3
Joanne Hoar <joanneh@bitheads.com>
2022-08-02 14:46:03 -0600

    Manually merge two additional files with origin/develop

--- RelayTestAppCPP/Source/RelayTestAppCPP/RelayTestAppCPP.Build.cs
+++ RelayTestAppCPP/Source/RelayTestAppCPP/RelayTestAppCPP.Build.cs
@@ -1,28 +1,3 @@
-<<<<<<< HEAD:DonutTag/Source/DonutTag/DonutTag.Build.cs
-// Fill out your copyright notice in the Description page of Project Settings.
-
-using UnrealBuildTool;
-
-public class DonutTag : ModuleRules
-{
-	public DonutTag(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
-}
-=======
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
@@ -48,4 +23,3 @@ public class RelayTestAppCPP : ModuleRules
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
->>>>>>> origin/develop:RelayTestAppCPP/Source/RelayTestAppCPP/RelayTestAppCPP.Build.cs
--- RelayTestAppCPP/Source/RelayTestAppCPP/Widgets/WidgetAddOns/OtherMatchUserWidget.cpp
+++ RelayTestAppCPP/Source/RelayTestAppCPP/Widgets/WidgetAddOns/OtherMatchUserWidget.cpp
@@ -1,14 +1,5 @@
-<<<<<<< HEAD:DonutTag/Source/DonutTag/DonutTag.h
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "CoreMinimal.h"
-
-=======
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "OtherMatchUserWidget.h"
 
->>>>>>> origin/develop:RelayTestAppCPP/Source/RelayTestAppCPP/Widgets/WidgetAddOns/OtherMatchUserWidget.cpp
