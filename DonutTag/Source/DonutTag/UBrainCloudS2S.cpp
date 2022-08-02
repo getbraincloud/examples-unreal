32db2cfef4e7f499ffd1493155fe4133d1674a51
Joanne Hoar <joanneh@bitheads.com>
2022-08-02 10:18:49 -0600

    relay server setup for DonutTag example

---
+++ DonutTag/Source/DonutTag/UBrainCloudS2S.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UBrainCloudS2S.h"
+
+
+UBrainCloudS2S::UBrainCloudS2S()
+{
+}
+
+UBrainCloudS2S::~UBrainCloudS2S()
+{
+}
