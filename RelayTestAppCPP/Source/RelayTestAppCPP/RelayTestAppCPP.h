a55318f938362be6331defb71cdd1c2288b5bd6a
Joanne Hoar <joanneh@bitheads.com>
2022-08-10 15:31:03 -0600

    CPP app doesn't need the struct since it can access #defines

--- RelayTestAppCPP/Source/RelayTestAppCPP/RelayTestAppCPP.h
+++ RelayTestAppCPP/Source/RelayTestAppCPP/RelayTestAppCPP.h
@@ -3,26 +3,4 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "ids.h"
-#include "RelayTestApp.generated.h"
 
-USTRUCT(BlueprintType)
-struct FAppIds
-{
-
-    GENERATED_BODY()
-    
-    FAppIds()
-    {
-        serverUrl = BRAINCLOUD_SERVER_URL;
-        secretKey = BRAINCLOUD_APP_SECRET;
-        appId = BRAINCLOUD_APP_ID;
-    }
-
-    UPROPERTY(BlueprintReadWrite)
-    FString serverUrl;
-    UPROPERTY(BlueprintReadWrite)
-    FString secretKey;
-    UPROPERTY(BlueprintReadWrite)
-    FString appId;
-};
