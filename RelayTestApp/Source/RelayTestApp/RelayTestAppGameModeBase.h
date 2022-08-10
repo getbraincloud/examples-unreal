5a6c0e354461496ad1438617f0fd2578ce1af0bf
Joanne Hoar <joanneh@bitheads.com>
2022-08-10 11:24:05 -0600

    RelayTestApp moved secret id to ids.h file

--- RelayTestApp/Source/RelayTestApp/RelayTestAppGameModeBase.h
+++ RelayTestApp/Source/RelayTestApp/RelayTestAppGameModeBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "ids.h"
 #include "RelayTestAppGameModeBase.generated.h"
 
 /**
@@ -14,4 +15,28 @@ class RELAYTESTAPP_API ARelayTestAppGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
-};
+
+    /**
+      * Get the secret ids for the app
+      */
+    UFUNCTION(BlueprintCallable, Category = "RelayTestApp5")
+    static FString getServerUrl(){
+          return TEXT(BRAINCLOUD_SERVER_URL);
+    }
+      /**
+        * Get the secret ids for the app
+        */
+      UFUNCTION(BlueprintCallable, Category = "RelayTestApp5")
+      static FString getAppId(){
+            return TEXT(BRAINCLOUD_APP_ID);
+      }
+      
+      /**
+        * Get the secret ids for the app
+        */
+      UFUNCTION(BlueprintCallable, Category = "RelayTestApp5")
+      static FString getSecretKey(){
+            return TEXT(BRAINCLOUD_APP_SECRET);
+      }
+  };
+
---
+++ RelayTestApp/Source/RelayTestApp/ids.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define BRAINCLOUD_SERVER_URL "https://api.internal.braincloudservers.com/dispatcherv2"
+#define BRAINCLOUD_APP_ID "23649"
+#define BRAINCLOUD_APP_SECRET "a754a2c0-72d9-46ce-9fdf-18e9c19a556c"
