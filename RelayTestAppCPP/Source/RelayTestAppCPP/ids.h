143a4c4cb0b91875c6059ac6cd8bc994e8643ea8
Joanne Hoar <joanneh@bitheads.com>
2022-08-21 04:07:40 -0600

    stop tracking ids.h to avoid conflicts

--- RelayTestAppCPP/Source/RelayTestAppCPP/ids.h
+++ RelayTestAppCPP/Source/RelayTestAppCPP/ids.h
@@ -1,10 +1,17 @@
 /*
  Do not commit modifications to this file in source control.
- Keep your app's secret key private.
+ Keep your app secret key private.
  Visit the BrainCloud Portal for info.
  */
 #pragma once
 
-#define BRAINCLOUD_SERVER_URL ""
-#define BRAINCLOUD_APP_ID ""
-#define BRAINCLOUD_APP_SECRET ""
+#define BRAINCLOUD_SERVER_URL "https://api.internal.braincloudservers.com/dispatcherv2"
+#define BRAINCLOUD_APP_ID "23649"
+#define BRAINCLOUD_APP_SECRET "a754a2c0-72d9-46ce-9fdf-18e9c19a556c"
+
+/*
+// prod
+#define BRAINCLOUD_SERVER_URL "https://api.braincloudservers.com/dispatcherv2"
+#define BRAINCLOUD_APP_ID "12944"
+#define BRAINCLOUD_APP_SECRET "35b0746e-fc38-499c-9b52-03533aac64a7"
+*/
--- TappyChicken/Source/TappyChicken/ids.h
+++ TappyChicken/Source/TappyChicken/ids.h
@@ -5,6 +5,13 @@
  */
 #pragma once
 
-#define BRAINCLOUD_SERVER_URL ""
-#define BRAINCLOUD_APP_ID ""
-#define BRAINCLOUD_APP_SECRET ""
+#define BRAINCLOUD_SERVER_URL "https://api.internal.braincloudservers.com/dispatcherv2"
+#define BRAINCLOUD_APP_ID "23232"
+#define BRAINCLOUD_APP_SECRET "844ecacd-e0d9-4aba-b78d-4c7fe9f2b22b"
+
+/*
+ // prod
+#define BRAINCLOUD_SERVER_URL "https://api.braincloudservers.com/dispatcherv2"
+#define BRAINCLOUD_APP_ID "23232"
+#define BRAINCLOUD_APP_SECRET "3aa89294-11fc-406b-aef5-d968cce7f77d"
+*/
