d105e26c61182bc9871f1967eab920e122a943a4
Joanne Hoar <joanneh@bitheads.com>
2022-09-03 07:46:22 -0600

    changing links in submodule to https for security

--- autobuild/update-submodules.sh
+++ autobuild/update-submodules.sh
@@ -13,7 +13,9 @@ then
     fi
 fi
 
-for i in RelayTestAppCPP/Plugins/BCClient RelayTestApp/Plugins/BCClient TappyChicken/Plugins/BCClient
+needspush=0
+
+for i in RelayTestAppCPP/Plugins/BCClient RelayTestApp/Plugins/BCClient TappyChicken/Plugins/BCClient BasicDemo5/Plugins/BCClient
 do
     echo
     git submodule status $i
@@ -44,14 +46,9 @@ do
             
             git add $i
             git commit -m "automatic submodules update" .
-            git push
-            code=$?
-            echo $code
-            if [[ $code != 0 ]];
-            then
-                echo "--- Failure $i needs updating and cannot push"
-                continue
-            fi
+
+            needspush=1
+
             git submodule status $i
             echo "--- $i updated on branch $STR"
          else
@@ -62,5 +59,17 @@ do
         echo "--- $i is already up to date on branch $STR"
     fi
 done
+
+if [[ $needspush != 0 ]];
+then
+    git push
+    code=$?
+    echo $code
+    if [[ $code != 0 ]];
+    then
+        echo "--- Failure: needs updating and cannot push"
+    fi
+fi
+
 echo
 exit $code
