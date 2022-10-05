239839cdc091496132a73a77085b4ccde5bf3a74
Joanne Hoar <joanneh@bitheads.com>
2022-10-05 11:26:56 -0600

    RelayTestApp env vars check for null return of char*

--- RelayTestApp/Source/RelayTestApp/RelayTestApp.h
+++ RelayTestApp/Source/RelayTestApp/RelayTestApp.h
@@ -18,10 +18,21 @@ struct FAppIds
         secretKey = BRAINCLOUD_APP_SECRET;
         appId = BRAINCLOUD_APP_ID;
         
-        if(appId=="")
-            appId = getenv("BC_RELAYTESTAPP_APP_ID");
-        if(secretKey=="")
-            secretKey = getenv("BC_RELAYTESTAPP_APP_SECRET");
+        if(appId==""){
+            char* env = getenv("BC_RELAYTESTAPP_APP_ID");
+            if(env != NULL)
+                appId = env;
+        }
+        if(secretKey==""){
+            char* env = getenv("BC_RELAYTESTAPP_APP_SECRET");
+            if(env != NULL)
+                secretKey = env;
+        }
+        if(serverUrl==""){
+            char* env = getenv("BC_BRAINCLOUD_SERVER_URL");
+            if(env != NULL)
+                serverUrl = env;
+        }
     }
 
     UPROPERTY(BlueprintReadWrite)
