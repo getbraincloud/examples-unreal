e28b1a3a4fc85b117af24ca34e9e085396c6ec87
Joanne Hoar <joanneh@bitheads.com>
2022-10-05 12:00:54 -0600

    checking for nulls, setting server url

--- RelayTestApp/Source/RelayTestApp/ids.h
+++ RelayTestApp/Source/RelayTestApp/ids.h
@@ -1,4 +1,4 @@
 #pragma once
-#define BRAINCLOUD_SERVER_URL "https://api.internal.braincloudservers.com/dispatcherV2"
+#define BRAINCLOUD_SERVER_URL ""
 #define BRAINCLOUD_APP_ID ""
 #define BRAINCLOUD_APP_SECRET ""
