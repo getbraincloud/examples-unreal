ea616a58c321c336e559a513807726500c6867e3
Nick H <nickh@bitheads.com>
2022-12-05 21:39:54 -0500

    Removed old duplicate project

--- BrainCloudFPSSample/Source/BrainCloudFPSSample/BrainCloudFPSSample.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2022 bitHeads, Inc. All Rights Reserved.
-
-#include "BrainCloudFPSSample.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BrainCloudFPSSample, "BrainCloudFPSSample" );
--- BrainCloudFPSSample/Source/BrainCloudFPSSample/BrainCloudFPSSample.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2022 bitHeads, Inc. All Rights Reserved.
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "Engine.h"
