422c789ada1822c364676ee52a4033b3e1e7d168
jo-codegirl <joanneh@bitheads.com>
2023-01-10 11:22:19 -0700

    playing with engine versions on mac scripts

--- autobuild/makebuild.sh
+++ autobuild/makebuild.sh
@@ -2,11 +2,13 @@
 # usage:
 #      autobuild/makebuild.sh project_name
 
-export PROJECTNAME=${1}
-export WORKSPACE=$PWD
+PROJECTNAME=$1
+WORKSPACE=$PWD
 
-"/Users/Shared/Epic Games/UE_5.1/Engine/Build/BatchFiles/Mac/GenerateProjectFiles.sh" -projectfiles -project="$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject" -game -rocket -progress 
+mkdir "${WORKSPACE}/MacOS-Build-${PROJECTNAME}"
 
-"/Users/Shared/Epic Games/UE_5.1/Engine/Build/BatchFiles/Mac/Build.sh" $PROJECTNAME Mac Development -Project="$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject" -WaitMutex
+#"/Users/Shared/Epic Games/UE_4.27/Engine/Build/BatchFiles/Mac/GenerateProjectFiles.sh" -projectfiles -project="${WORKSPACE}/${PROJECTNAME}/${PROJECTNAME}.uproject" -game -rocket -progress 
 
-"/Users/Shared/Epic Games/UE_5.1/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -rocket -nocompile -compileeditor -installed -nop4 -project="$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject" -cook -stage -archive -archivedirectory="$WORKSPACE/$PROJECTNAME_MACOS_Build" -package -clientconfig=Development -clean -pak -prereqs -distribution -nodebuginfo -targetplatform=Mac -build -utf8output 
+#"/Users/Shared/Epic Games/UE_4.27/Engine/Build/BatchFiles/Mac/Build.sh" $PROJECTNAME Mac Development -Project="${WORKSPACE}/${PROJECTNAME}/${PROJECTNAME}.uproject" -WaitMutex
+
+"/Users/Shared/Epic Games/UE_4.27/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -rocket -nocompile -compileeditor -installed -nop4 -project="${WORKSPACE}/${PROJECTNAME}/${PROJECTNAME}.uproject" -cook -stage -archive -archivedirectory="${WORKSPACE}/MacOS-Build-${PROJECTNAME}" -package -clientconfig=Development -clean -pak -prereqs -distribution -nodebuginfo -targetplatform=Mac -build -utf8output 
