2e6122ca2221ee1170707f09e2b0ca85c6ecf37b
jo-codegirl <joanneh@bitheads.com>
2023-01-31 16:07:47 -0700

    builld script for UE5 examples

--- autobuild/makebuild.sh
+++ autobuild/makebuild.sh
@@ -2,7 +2,7 @@
 # usage:
 #      autobuild/makebuild.sh project_name
 # make sure to set the install path of your engine version
-#       export UE_INSTALL_PATH='/Users/Shared/Epic Games/UE_4.27'
+#       export UE_INSTALL_PATH='/Users/Shared/Epic Games/UE_5.1'
 
 PROJECTNAME=${1}
 WORKSPACE=$PWD
@@ -15,5 +15,6 @@ mkdir "$WORKSPACE/Binaries/${PROJECTNAME}_MacOSBuild"
 
 "${UE_INSTALL_PATH}/Engine/Build/BatchFiles/Mac/Build.sh" ${PROJECTNAME}Editor Mac Development -Project="$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject" 
 
-"${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -nocompileeditor -installed -nop4 -project="$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject" -cook -stage -archive -archivedirectory="$WORKSPACE/MacOS_Build_${PROJECTNAME}" -package  -ue4exe="/Users/Shared/Epic Games/UE_4.27/Engine/Binaries/Mac/UE4Editor.app/Contents/MacOS/UE4Editor"  -compressed -SkipCookingEditorContent -clientconfig=Development -clean -pak -prereqs -distribution -nodebuginfo -targetplatform=Mac -build -target=RelayTestApp -utf8output 
+"${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -nocompileeditor -installed -nop4 -project="$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject" -cook -stage -archive -archivedirectory="$WORKSPACE/MacOS_Build_${PROJECTNAME}" -package   -compressed -SkipCookingEditorContent -clientconfig=Development -clean -pak -prereqs -distribution -nodebuginfo -targetplatform=Mac -build -target=RelayTestApp -utf8output 
  
+ #-ue4exe="/Users/Shared/Epic Games/UE_4.27/Engine/Binaries/Mac/UE4Editor.app/Contents/MacOS/UE4Editor"
