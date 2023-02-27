7bfe3cd4d12c7e5288af1f58599829cc4267570a
jo-codegirl <joanneh@bitheads.com>
2023-02-27 14:26:21 -0700

    Updated TappyChicken to UE 5.1

---
+++ autobuild/switch_project.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# switch project to unreal version - don't do this while Unreal Editor or xCode are running, right
+# usage:
+#     source autobuild/switch_project.bat 5.0
+#     source autobuild/switch_project.bat
+#     source autobuild/switch_project.bat 4.27
+
+UE_VERSION=$1
+if [ -z "${UE_VERSION}" ]; then UE_VERSION="5.1"; fi
+
+PROJECT_NAME=$2
+
+# old intermediate files will be out of date
+../autobuild/cleanupunreal.sh
+
+# TODO: copy in config changes and re-generate xcode/vs project files
+
+#get correct VaRest
+cd Plugins/VaRest
+if [ $UE_VERSION == "4.27" ];
+then
+	git checkout 4.27
+else 
+	if [ $UE_VERSION == "5.0" ];
+	then
+		git checkout 5.0
+	else 
+		git checkout develop
+	fi
+fi
+git pull
+cd ../..
+
+echo "Updated plugin VaRest" 
+
+if [ $UE_VERSION == "4.27" ];
+then
+	export UE_INSTALL_PATH='/Users/Shared/Epic Games/UE_4.27'
+	export UE_EDITOR_CMD='UE4Editor-Cmd'
+else 
+	if [ $UE_VERSION == "5.0" ];
+	then
+		export UE_INSTALL_PATH='/Users/Shared/Epic Games/UE_5.0'
+		export UE_EDITOR_CMD='UE4Editor-Cmd'
+	else 
+		export UE_INSTALL_PATH='/Users/Shared/Epic Games/UE_5.1'
+		export UE_EDITOR_CMD='UnrealEditor-Cmd'
+	fi
+fi
+
+echo "Updated path to ${UE_INSTALL_PATH}" 
+		
+# replace version in .uproject
+ sed -i '' "s/\"EngineAssociation\": \".*$/\"EngineAssociation\": \"${UE_VERSION}\",/g" ${PROJECT_NAME}.uproject
+
+../autobuild/workspace-genarate.sh ${PROJECT_NAME}
+
+echo "Project files have been modified for UE ${UE_VERSION}"
--- autobuild/workspace-genarate.sh
+++ autobuild/workspace-genarate.sh
@@ -3,6 +3,5 @@
 #      autobuild/makebuild.sh project_name
 
 export PROJECTNAME=${1}
-export WORKSPACE=$PWD
 
 "/Users/Shared/Epic Games/UE_5.1/Engine/Build/BatchFiles/Mac/GenerateProjectFiles.sh" -projectfiles -project="$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject" -game -rocket -progress 
