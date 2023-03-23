968ec6e3d75245fc1c82442f9e0430e7b4c669a5
jo-codegirl <joanneh@bitheads.com>
2023-03-23 08:24:28 -0600

    Fix issue with groups config

--- autobuild/generate-config.sh
+++ autobuild/generate-config.sh
@@ -3,21 +3,21 @@ APP_ID=${2}
 SECRET_KEY=${3}
 S2S_KEY=${4}
 
-LINES=5
-
 cd Config
 
 sed -i '' "s/SecretKey = .*$/SecretKey = \"${SECRET_KEY}\"/g" DefaultEngine.ini
 sed -i '' "s/AppID = .*$/AppID = \"${APP_ID}\"/g" DefaultEngine.ini
 
-if [ -z S2S_Key ];
+if [ -z $S2S_KEY ];
 then
+LINES=5
+else
 sed -i '' "s/S2SKey = .*$/S2SKey = \"${S2S_KEY}\"/g" DefaultEngine.ini
 LINES=7
 fi
 
 cd ..
 
-echo "File modified Config/DefaultEngine.ini"
+echo "$PROJECT_NAME config modified Config/DefaultEngine.ini"
 head -${LINES} Config/DefaultEngine.ini
 echo "-- Please do not commit these changes to git"
