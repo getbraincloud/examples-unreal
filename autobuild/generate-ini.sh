511cc55bbde6aacb5439cc5599955586c89acfb6
jo-codegirl <joanneh@bitheads.com>
2023-03-23 05:32:38 -0600

    scripts to gernerate ids

---
+++ autobuild/generate-ini.sh
@@ -0,0 +1,23 @@
+PROJECT_NAME=${1?Error}
+APP_ID=${2}
+SECRET_KEY=${3}
+S2S_KEY=${4}
+
+LINES=5
+
+cd Config
+
+sed -i '' "s/SecretKey = .*$/SecretKey = \"${SECRET_KEY}\"/g" DefaultEngine.ini
+sed -i '' "s/AppID = .*$/AppID = \"${APP_ID}\"/g" DefaultEngine.ini
+
+if [ S2S_Key != "" ];
+then
+sed -i '' "s/S2SKey = .*$/S2SKey = \"${S2S_KEY}\"/g" DefaultEngine.ini
+LINES=7
+fi
+
+cd ..
+
+echo "File modified Config/DefaultEngine.ini"
+head -${LINES} Config/DefaultEngine.ini
+echo "-- Please do not commit these changes to git"
