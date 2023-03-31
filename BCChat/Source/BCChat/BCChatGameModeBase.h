e6498d8bf5d938187fe2d6a42c39a8b48fbaef5b
franco <82105876+francobithead@users.noreply.github.com>
2023-03-31 16:05:36 -0400

    Stuff and things, with things and stuff
    Lots of fighting with Unreal to implement a single C++ class. I lost and now it doesn't compile normally like other examples do.

---
+++ BCChat/Source/BCChat/BCChatGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BCChatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BCCHAT_API ABCChatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+};
