9472105086010d7bbc8c050783e4f738cff9ba20
jo-codegirl <joanneh@bitheads.com>
2023-06-19 14:58:59 -0600

    build script

--- autobuild/makebuild.sh
+++ autobuild/makebuild.sh
@@ -14,11 +14,11 @@
 
 PROJECTNAME=${1}
 TARGET=${2}
-if [ $UE_VERSION == 4.27 ];
+if ][ $UE_VERSION == "4.27" ]];
 then
 	UE4String='-ue4exe="${UE_INSTALL_PATH}/Engine/Binaries/Mac/UE4Editor.app/Contents/MacOS/UE4Editor"'
 fi
-if [ "$3" !=  "nosign" ];
+if [[ "$3" !=  "nosign" ]];
 then
 	ModeString='-distribution'
 fi
@@ -30,6 +30,6 @@ mkdir -p "$WORKSPACE/artifacts/${PROJECTNAME}-${TARGET}"
 
 "${UE_INSTALL_PATH}/Engine/Build/BatchFiles/Mac/Build.sh" ${PROJECTNAME}Editor Mac Development -Project="$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject" 
 
-"${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -nocompileeditor -installed -nop4 -project="$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject" -cook -stage -archive -archivedirectory="$WORKSPACE/artifacts/${PROJECTNAME}-${TARGET}" -package   -compressed -SkipCookingEditorContent -clientconfig=Development -clean -pak ${ModeString} -prereqs -nodebuginfo ${UE4String} -targetplatform=${TARGET} -build -target=${PROJECTNAME} -utf8output 
+#"${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -nocompileeditor -installed -nop4 -project="$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject" -cook -stage -archive -archivedirectory="$WORKSPACE/artifacts/${PROJECTNAME}-${TARGET}" -package   -compressed -SkipCookingEditorContent -clientconfig=Development -clean -pak ${ModeString} -prereqs -nodebuginfo ${UE4String} -targetplatform=${TARGET} -build -target=${PROJECTNAME} -utf8output 
  
- #
+ "/Users/Shared/Epic Games/UE_5.1/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -rocket -nocompile -compileeditor -installed -nop4 -project="$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject" -cook -stage -archive -archivedirectory="$WORKSPACE/artifacts/${PROJECTNAME}-${TARGET}" -package -clientconfig=Development -clean -pak -prereqs -distribution -nodebuginfo -targetplatform=${TARGET} -build -target=${PROJECTNAME} -utf8output 
