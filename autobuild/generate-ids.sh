159588fd2104fd891be3f929ee3c18bbb4186b67
jo-codegirl <joanneh@bitheads.com>
2023-06-19 06:26:48 -0600

    ids and configs generating (bash)

--- autobuild/generate-ids.sh
+++ autobuild/generate-ids.sh
@@ -1,20 +1,22 @@
 PROJECT_NAME=${1?Error}
 APP_ID=${2}
 SECRET_KEY=${3}
+LOG=false
 
 cd Source/$PROJECT_NAME
 
-
 echo "#pragma once" >ids.h
 
-
-echo "#define BRAINCLOUD_SERVER_URL \"https://api.internal.braincloudservers.com/dispatcherv2\"" >>ids.h
-echo "#define BRAINCLOUD_APP_ID \"$APP_ID\"" >>ids.h
-echo "#define BRAINCLOUD_APP_SECRET \"$SECRET_KEY\"" >>ids.h
+if [ $LOG == true ];
+then
+	echo "#define BRAINCLOUD_SERVER_URL \"https://api.internal.braincloudservers.com/dispatcherv2\"" >>ids.h
+	echo "#define BRAINCLOUD_APP_ID \"$APP_ID\"" >>ids.h
+	echo "#define BRAINCLOUD_APP_SECRET \"$SECRET_KEY\"" >>ids.h
+fi 
 
 echo "File ids.h created in Source/$PROJECT_NAME"
 
 cat ids.h
 
-git update-index --skip-worktree ids.h
+git update-index  --assume-unchanged ids.h
 echo "-- File ids.h excluded from git worktree"
