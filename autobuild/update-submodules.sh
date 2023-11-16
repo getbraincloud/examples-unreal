bbeacb92444240562c9e6b78150ea21791791bc2
jo-codegirl <joanneh@bitheads.com>
2023-11-16 08:46:16 -0700

    update submodule script for BCChat

--- autobuild/update-submodules.sh
+++ autobuild/update-submodules.sh
@@ -15,7 +15,7 @@ fi
 
 needspush=0
 
-for i in RelayTestAppCPP/Plugins/BCClient RelayTestApp/Plugins/BCClient TappyChicken/Plugins/BCClient ScriptTestApp/Plugins/BCClient Groups/Plugins/BCClient Leaderboard/Plugins/BCClient
+for i in BCChat/Plugins/BCClient RelayTestAppCPP/Plugins/BCClient RelayTestApp/Plugins/BCClient TappyChicken/Plugins/BCClient ScriptTestApp/Plugins/BCClient Groups/Plugins/BCClient Leaderboard/Plugins/BCClient
 do
     echo
     git submodule status $i
