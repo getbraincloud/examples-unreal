9fff4313a10b9bb8dda667f4b433bafb5e7b0a64
franco <82105876+francobithead@users.noreply.github.com>
2023-11-23 14:13:35 -0500

    Testing Friend Request Solution

--- BCFPS/Source/BCFPS/BCHelpers.h
+++ BCFPS/Source/BCFPS/BCHelpers.h
@@ -96,6 +96,9 @@ struct FBrainCloudPlayerData
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Data")
 		FLobbyMemberData LobbyMemberData;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Data")
+		FString ProfileID = "";
 };
 
 USTRUCT(BlueprintType)
