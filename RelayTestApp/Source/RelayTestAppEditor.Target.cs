6080146c85ab8afc8403a78371ba121d56e58589
jo-codegirl <joanneh@bitheads.com>
2024-01-23 13:42:43 -0700

    fixed relay test app launch sequence (null pointers on packaged build), fixed crash on launch with no settings file - upgrade to UE 5.3!

--- RelayTestApp/Source/RelayTestAppEditor.Target.cs
+++ RelayTestApp/Source/RelayTestAppEditor.Target.cs
@@ -8,16 +8,8 @@ public class RelayTestAppEditorTarget : TargetRules
 	public RelayTestAppEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.AddRange( new string[] { "RelayTestApp" } );
-
-        if (Target.Platform == UnrealTargetPlatform.Mac)
-        {
-            // use this compiler flag for Xcode 14 (Apple clang version 14.0.0)
-            // commandline option to avoid errors in Unreal source
-            // not available in Xcode 13.2 or less (Apple clang version 13.0.0)
-            bOverrideBuildEnvironment = true;
-            AdditionalCompilerArguments = "-Wno-unused-but-set-variable";
-        }
       }
 }
