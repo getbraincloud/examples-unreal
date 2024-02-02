c7864685d00b89063b95ab2052105fbeaab6360f
jo-codegirl <joanneh@bitheads.com>
2024-02-02 09:48:12 -0700

    error in script

--- autobuild/workspace-generate.bat
+++ autobuild/workspace-generate.bat
@@ -2,6 +2,6 @@
 :: usage:
 ::      autobuild\workspace-generate.bat project_name
 
-PROJECTNAME=%1
+set PROJECTNAME=%1
 echo %PROJECTNAME%
 call "%UE_INSTALL_PATH%\Engine\Binaries\DotNet\UnrealBuildTool\UnrealBuildTool.exe" -projectfiles -project="%WORKSPACE%\%PROJECTNAME%\%PROJECTNAME%.uproject" -game -rocket -progress
