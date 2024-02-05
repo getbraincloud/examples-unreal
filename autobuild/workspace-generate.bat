297b4acae13da8be157a4e37246f8962d689c0ab
jo-codegirl <joanneh@bitheads.com>
2024-02-05 13:14:20 -0700

    typo in script for 4.27

--- autobuild/workspace-generate.bat
+++ autobuild/workspace-generate.bat
@@ -5,7 +5,6 @@
 set PROJECTNAME=%1
 echo %PROJECTNAME%
 if [%UE_VERSION%]==[4.27] (
-then
     call "%UE_INSTALL_PATH%\Engine\Binaries\DotNet\UnrealBuildTool.exe" -projectfiles -project="%WORKSPACE%\%PROJECTNAME%\%PROJECTNAME%.uproject" -game -rocket -progress
 ) else (
     call "%UE_INSTALL_PATH%\Engine\Binaries\DotNet\UnrealBuildTool\UnrealBuildTool.exe" -projectfiles -project="%WORKSPACE%\%PROJECTNAME%\%PROJECTNAME%.uproject" -game -rocket -progress
