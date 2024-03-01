d0a73bd70601e03a7fc6ee1b332b7ec4da91ff49
jo-codegirl <joanneh@bitheads.com>
2024-03-01 06:36:36 -0700

    updated build scripts, BCChat adding certificates to package build for mac

--- autobuild/build-all.sh
+++ autobuild/build-all.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-export archive=${1:-.}
+# USAGE:
+#   ./autobuild/build-all.sh
+#   ./autobuild/build-all.sh "/Users/<myusername>/Library/CloudStorage/GoogleDrive-<myuseremail>/Shared drives/<BuildFolder>"
+#   ./autobuild/build-all.sh NewBuildFolder
 
-rm -rf artifacts/*
+export archive=${1}
+if [ -z "${1}" ];
+then
+  export archive="."
+  else
+    export archive=${1}
+fi
+
+if [ -d "$archive" ];
+then
+  echo Writing ZIP files to $archive
+  else
+    echo Creating folder for ZIP files at $archive
+    mkdir "$archive"
+fi
 
 for i in BCFPS MobileTestApp BCChat RelayTestApp TappyChicken ScriptTestApp Groups Leaderboard
 do
   export target=Mac
 	export proj=$i
 	autobuild/makebuild.sh ${proj} ${target}
-	cd artifacts
 	if [ -d ${proj}_Unreal_${target}Build ];
 	then
+    cd artifacts
   	cp ../../../data/quarantine.command ${proj}_Unreal_${target}Build
-	  zip -r $archive/${proj}_Unreal_MacBuild.zip ${proj}_Unreal_${target}Build
+	  zip -r ${proj}_Unreal_${target}Build.zip ${proj}_Unreal_${target}Build
+  	cd ..
+  	mv ${proj}_Unreal_${target}Build.zip "$archive"
 	fi
-	cd ..
 done
 
 for i in MobileTestApp BCChat RelayTestApp TappyChicken ScriptTestApp Groups Leaderboard
@@ -23,26 +41,27 @@ do
   export target=Android
 	export proj=$i
 	autobuild/makebuild.sh ${proj} ${target}
-	cd artifacts
 	if [ -d ${proj}_Unreal_${target}Build ];
 	then
+    cd artifacts
   	cp ../../../data/quarantine.command ${proj}_Unreal_${target}Build
-	  zip -r $archive/${proj}_Unreal_MacBuild.zip ${proj}_Unreal_${target}Build
+	  zip -r ${proj}_Unreal_${target}Build.zip ${proj}_Unreal_${target}Build
+  	cd ..
+  	mv ${proj}_Unreal_${target}Build.zip "$archive"
 	fi
-	cd ..
 done
 
-
-for i in MobileTestApp BCChat RelayTestApp
+for i in MobileTestApp BCChat RelayTestApp TappyChicken ScriptTestApp Groups Leaderboard
 do
 	export proj=$i
   export target=IOS
 	autobuild/makebuild.sh ${proj} ${target}
-	cd artifacts
 	if [ -d ${proj}_Unreal_${target}Build ];
 	then
+    cd artifacts
   	cp ../../../data/quarantine.command ${proj}_Unreal_${target}Build
-	  zip -r $archive/${proj}_Unreal_MacBuild.zip ${proj}_Unreal_${target}Build
+	  zip -r ${proj}_Unreal_${target}Build.zip ${proj}_Unreal_${target}Build
+  	cd ..
+  	mv ${proj}_Unreal_${target}Build.zip "$archive"
 	fi
-	cd ..	
 done
