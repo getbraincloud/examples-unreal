da9322235a4d71a7b7b0ed78fd0b60aa6dfb5de6
Jo Hoar <joanneh@bitheads.com>
2024-05-03 13:28:00 -0400

    UE4TestApp: Updates to UI for printing and test logic; update functions library

--- UE4TestApp/Source/UE4TestApp/MyBlueprintFunctionLibrary.cpp
+++ UE4TestApp/Source/UE4TestApp/MyBlueprintFunctionLibrary.cpp
@@ -7,11 +7,20 @@
 FString UMyBlueprintFunctionLibrary::RunCppFunction(UBrainCloudWrapper* wrapper)
 {
 	FString Ret("");
-	if(wrapper != nullptr)
-	    Ret = FString::Printf(TEXT("Wrapper initialized"));
+	if (wrapper == nullptr) return Ret;
+	  
     // add some developer code here
+	FString locale = FPlatformMisc::GetDefaultLocale();
 
-	 return Ret;
+	FCulturePtr culture = FInternationalization::Get().GetCulture(locale);
+
+	if (culture.IsValid())
+		Ret = culture->GetRegion();
+
+	else
+		Ret = "fooey";
+
+	return Ret;
 }
 
 FString UMyBlueprintFunctionLibrary::GetCountryOverride()
@@ -30,12 +39,29 @@ void UMyBlueprintFunctionLibrary::RunCppCountryOverride(UBrainCloudWrapper* wrap
 	FString CountryCode("");
 	// add some developer code here
 
-	if (!CountryCode.IsEmpty()) {
-		wrapper->getClient()->overrideCountryCode(CountryCode);
+	if (CountryCode.IsEmpty() && wrapper->getClient()->getCountryCode().IsEmpty()) {
+		// this is already the default brainCloud client initialization
+		// see FSonyPlatformMisc::GetDefaultLocale()
+		FString locale = FPlatformMisc::GetDefaultLocale();
+		
+		FString language, country;
+		locale.Split(TEXT("-"), &language, &country);
+
+		if (country.IsEmpty())
+			CountryCode = language;
+		else 
+			CountryCode = country;
+		
+		if (CountryCode == "419") {
+			CountryCode = "_LA_";
+		}
+		else if ((CountryCode == "Hans") || (CountryCode == "Hant")) {
+			CountryCode = "CN";
+		}
 	}
-	else if(wrapper->getClient()->getCountryCode().IsEmpty()) {
-		// this is also the brainCloudF client default
-		wrapper->getClient()->overrideCountryCode(FPlatformMisc::GetDefaultLocale());
-	}
+
+	CountryCode = CountryCode.ToUpper();
+	wrapper->getClient()->overrideCountryCode(CountryCode);
 }
+
 
