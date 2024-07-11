30c702aee798f361645e92caca57a8a4471ad3c8
SpencerH <spencerh@bitheads.com>
2024-07-11 17:10:45 -0400

    Added Clipping Mask, Added Function Library

---
+++ RelayTestApp/Source/RelayTestApp/Private/SplatterAnimationLibrary.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SplatterAnimationLibrary.h"
+
+
+float USplatterAnimationLibrary::SplatSizeOverTime(float t, float a, float b) 
+{
+	float grow = (1 + b) * t / a;
+	float shrink = -(((1 + b) * t) - ((2 + b) * a)) / a;
+	return std::min(grow, shrink);
+}
+
+float USplatterAnimationLibrary::SplatOpacityOverTime(float t, float a) 
+{
+	return std::min(t / a, 0.25f);
+}
+
+float USplatterAnimationLibrary::FadeOpacityOverTime(float t, float a)
+{
+	return 1 - pow(t / a, 2);
+}
---
+++ RelayTestApp/Source/RelayTestApp/Public/SplatterAnimationLibrary.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SplatterAnimationLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RELAYTESTAPP_API USplatterAnimationLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Splatter")
+	static float SplatSizeOverTime(float t, float a, float b);
+
+	UFUNCTION(BlueprintCallable, Category = "Splatter")
+	static float SplatOpacityOverTime(float t, float a);
+
+	UFUNCTION(BlueprintCallable, Category = "Splatter")
+	static float FadeOpacityOverTime(float t, float a);
+};
