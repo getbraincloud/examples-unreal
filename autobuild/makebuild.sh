81b58ff8b3fb8dd3af08583da96f0ca5f2d81c7b
Nick H <nickh@bitheads.com>
2025-09-03 12:37:41 -0400

    Updated build script to add architecture flag

--- autobuild/makebuild.sh
+++ autobuild/makebuild.sh
@@ -60,7 +60,7 @@ esac
 #flags for testing PCH
 # -noPCH -NoSharedPCH -DisableUnity
 
-"${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -project=$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject -noP4 -nocompile -utf8output -compileeditor -platform=${TARGET} ${ModeString} -clientconfig=Development -serverconfig=Development -build -cook ${CultureString} ${CookString} -unversionedcookedcontent -pak -compressed -iostore -nodebuginfo -stage -iterate -prereqs -installed -nocompileuat -package -archive -archivedirectory="${ARTIFACTS}/${PROJECTNAME}_${SERVERENV}_${TARGET}_${BUILDNUMBER}"
+"${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -project=$WORKSPACE/$PROJECTNAME/$PROJECTNAME.uproject -noP4 -nocompile -utf8output -compileeditor -architecture=arm64 -platform=${TARGET} ${ModeString} -clientconfig=Development -serverconfig=Development -build -cook ${CultureString} ${CookString} -unversionedcookedcontent -pak -compressed -iostore -nodebuginfo -stage -iterate -prereqs -installed -nocompileuat -package -archive -archivedirectory="${ARTIFACTS}/${PROJECTNAME}_${SERVERENV}_${TARGET}_${BUILDNUMBER}"
 
 retcode=$?
 
